ca954f9750a8be57f90f8603086f7e3675cb745a
DmnkHofmann <dmnkhofmann@gmail.com>
2022-07-28 09:02:15 +0200

    finished

---
+++ Dominik/tests/FileTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace TDD\Test;
+// declare(strict_types=1);
+use PHPUnit\Framework\TestCase; // imports PHPUnit core class called TestCase
+use TDD\SrcFile; // imports SrcFile class
+// Create Test Class
+
+/*
+* @covers  TDD\SrcFile
+*/
+class FileTest extends  TestCase {
+
+  public function  testSum() {
+
+      $obj = new SrcFile(); // dummy object for testing
+      $input = [ 0, 2, 5, 8, 2]; // put the inputs in its own array
+      $output = $obj->sum($input); // store the sum of the array within a $output variable
+       $this->assertEquals( // Built-in assertEquals() method for defining the test. It accepts three parameters
+           17, // 1st param is the expected value
+          $output, // 2nd param is the $output variable
+          'When summing the total should equal 15' // 3rd param is a message displayed in case of failure
+      );
+  }
+}
